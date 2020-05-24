6bea07e68cb1abda19cb8897451c7e06922f04c1
Alan-Pardoen <280202@supinfo.com>
2020-05-24 14:40:13 +0200

    Comand pannel

--- src/components/Command.vue
+++ src/components/Command.vue
@@ -1,5 +1,24 @@
 <template>
-    
+    <div class="command"> 
+        <b-tabs fill> 
+            <b-tab title = "Web" class="web"  active >
+                <b-list-group class="px-2 py-2" > 
+                    <b-list-group-item  href = "#" > Commande 1 </b-list-group-item > 
+                    <b-list-group-item  href = "#" class="striped"> Commande 2 </b-list-group-item > 
+                    <b-list-group-item  href = "#" > Commande 3 </b-list-group-item >
+                    <b-list-group-item  href = "#" class="striped"> Commande 4 </b-list-group-item >  
+                </b-list-group > 
+            </b-tab > 
+            <b-tab title = "Disk" class="disk" > 
+                <b-list-group class="px-2 py-2" > 
+                    <b-list-group-item  href = "#" > Commande 1 </b-list-group-item > 
+                    <b-list-group-item  href = "#" class="striped" > Commande 2 </b-list-group-item > 
+                    <b-list-group-item  href = "#" > Commande 3 </b-list-group-item > 
+                </b-list-group >  
+            </b-tab > 
+            <b-tab title = "Serveur"> <p> Serveur</p> </b-tab> 
+        </b-tabs> 
+    </div>
 </template>
 
 <script>
@@ -9,5 +28,9 @@ export default {
 </script>
 
 <style lang="scss">
-
+    .command{
+        .striped{
+            background-color: rgba(0, 0, 0, 0.05);
+        }
+    }
 </style>
